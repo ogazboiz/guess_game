00e23cbc3f0bb1f88261b31dd3482fd3a35ebc53
ogazboiz <ogazboizakpolo@gmail.com>
2023-01-07 03:27:23 +0100

    done with all

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let randomNumber = Math.trunc(Math.random() * 20 + 1);
 console.log(randomNumber);
 document.querySelector('.number').textContent = '?';
 let scoreBoard = 20;
-const restart = document.querySelector('.again');
-document.querySelector('.check').addEventListener('click', function (e) {
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
   console.log();
   const guess = Number(document.querySelector('.guess').value);
   const Checknumber = document.querySelector('.message');
@@ -15,6 +16,10 @@ document.querySelector('.check').addEventListener('click', function (e) {
     document.querySelector('.message').textContent = 'you won 🏆';
     document.querySelector('body').style.background = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (scoreBoard > highScore) {
+      highScore = scoreBoard;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > randomNumber) {
     if (scoreBoard > 1) {
       document.querySelector('.message').textContent = 'go lower ';
@@ -37,3 +42,10 @@ document.querySelector('.check').addEventListener('click', function (e) {
     }
   }
 });
+const restart = function () {
+  document.querySelector('.score').textContent = '20';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+};
+
+document.querySelector('.again').addEventListener('click', restart);
